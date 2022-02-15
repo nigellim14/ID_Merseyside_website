94f0aa1ca28532b37a77660595568bb94c034144
nigellim14 <S10208206@connect.np.edu.sg>
2022-02-15 16:23:48 +0800

    Implemented a Collapse for campus map

--- script.js
+++ script.js
@@ -229,7 +229,22 @@ $(document).ready(function () {
   }
 
 
+/* Collapsible message for map */
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
 
  
